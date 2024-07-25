08aef181c0a961e15ea21917323f1b17f89d061d
Guilherme Souza <guilhermesouzae21@gmail.com>
2024-07-25 00:12:08 -0300

    feat: event selector

---
+++ ui/EventSelector/index.js
@@ -0,0 +1,41 @@
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import React from 'react';
+import { Communities } from '../../communities/communities';
+
+export function EventSelector({ selectedEvent, handleEventChange }) {
+  const { communities, loading } = useTracker(() => {
+    const data = { communities: [], loading: true };
+
+    const handle = Meteor.subscribe('communities');
+
+    if (handle.ready()) {
+      data.communities = Communities.find({}).fetch();
+      data.loading = false;
+      return data;
+    }
+
+    return data;
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="mx-auto pt-4">
+      <select
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+        defaultValue={selectedEvent}
+        onChange={handleEventChange}
+      >
+        <option value={null}>Select an event</option>
+        {communities.map((c) => (
+          <option key={c._id} value={c._id}>
+            {c.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
