877d7fc47036fa6975a7020cfcaecc41a9c3f12c
Guilherme Souza <guilhermesouzae21@gmail.com>
2024-07-26 11:23:57 -0300

    styles

--- ui/EventSelector/index.js
+++ ui/EventSelector/index.js
@@ -26,9 +26,9 @@ export function EventSelector() {
   }
 
   return (
-    <div className="mx-auto mt-4">
+    <div className="mt-4">
       <select
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+        className="w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
         defaultValue={selectedEvent}
         onChange={updateSelectedEvent}
       >
